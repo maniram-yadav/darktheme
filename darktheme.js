b20e35d28ea9eb4fca8a8a5619e674a36d5dfcfa
maniram <maniramyadav786@gmail.com>
2025-07-08 13:04:15 +0530

    toggle button implemntation

--- darktheme.js
+++ darktheme.js
@@ -3,17 +3,24 @@ console.log(theme);
 if(theme){
     document.body.style.transition='none';
     document.body.classList.add('dark-theme');
+    document.querySelector('.btndark')
+            .style.setProperty('--right', 40);
 }
 
 function toggleTheme(){
     
     const theme = localStorage.getItem('theme');
+
     if(theme==='dark-theme'){
         document.body.classList.remove('dark-theme');
         localStorage.setItem('theme', '');
+        document.querySelector('.btndark')
+            .style.setProperty('--right', 0);
     } else{
         document.body.classList.add('dark-theme');
         localStorage.setItem('theme', 'dark-theme');
+        document.querySelector('.btndark')
+            .style.setProperty('--right', 40);
     }
     
     
